e9ab910d58538394c033e96a911e56aa92d77ed3
Lee Collings <lee@leecollings.co>
2016-07-28 11:18:12 +0100

    Removed hardcoded link to CSS, moved to `wp_enqueue_style`

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
