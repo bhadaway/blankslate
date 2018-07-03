18bc7a3ebffeeab371ba29d3bc13802e85f46891
Annie Sullivan <sullivan@chromium.org>
2018-07-02 22:16:55 -0400

    Add material design searchform.

---
+++ searchform.php
@@ -0,0 +1,14 @@
+
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+									mdl-textfield--floating-label mdl-textfield--align-right">
+				<form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+	        <label class="mdl-button mdl-js-button mdl-button--icon"
+  	             for="waterfall-exp">
+    	      <i class="material-icons">search</i>
+      	  </label>
+        	<div class="mdl-textfield__expandable-holder">
+          	<input class="mdl-textfield__input" type="text" name="s"
+            	     id="waterfall-exp">
+					</div>
+				</form>
+      </div>
