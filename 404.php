170f4f22c84de670ae4474f51a34e405d246818e
Annie Sullivan <sullivan@chromium.org>
2018-07-03 16:06:46 -0400

    Formatting for 404 page.

--- 404.php
+++ 404.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Not Found', 'barebones-mdl' ); ?></h1>
-</header>
-<section class="entry-content">
-<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'barebones-mdl' ); ?></p>
-<?php get_search_form(); ?>
-</section>
-</article>
+<section id="content" role="main" class="mdl-grid">
+	<article id="post-0" class="post not-found mdl-cell--12-col mdl-card mdl-shadow--4dp">
+		<header class="header mdl-card__title mdl-card--expand">
+			<h1 class="entry-title"><?php _e( 'Not Found', 'barebones-mdl' ); ?></h1>
+		</header>
+		<section class="entry-content mdl-card__supporting-text">
+			<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'barebones-mdl' ); ?></p>
+			<?php get_search_form(); ?>
+		</section>
+	</article>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
