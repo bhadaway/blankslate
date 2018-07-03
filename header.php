cc7403d4b4f7b56c2092ff24f5149d2d7e992429
Annie Sullivan <sullivan@chromium.org>
2018-07-03 13:26:58 -0400

    Added material design menu. Enclosed content as recommended. Fixed footer formatting.

--- header.php
+++ header.php
@@ -7,10 +7,15 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.deep_purple-cyan.min.css" />
 	<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+	<style>
+		.layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type  {
+			padding-right: 0;
+		}
+	</style>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<div id="wrapper" class="hfeed demo-layout-waterfall mdl-layout mdl-js-layout"">
+	<div id="wrapper" class="hfeed layout-waterfall mdl-layout mdl-js-layout"">
 		<header id="header" role="banner" class="mdl-layout__header mdl-layout__header--waterfall">
 			<section id="branding" class="mdl-layout__header-row">
 				<div id="site-title" class="mdl-layout-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
@@ -28,4 +33,13 @@
 				 ) ); ?>
 			</div>
 		</header>
-		<div id="container">
+		<div class="mdl-layout__drawer">
+			<span class="mdl-layout-title"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
+			<?php wp_nav_menu( array(
+				'theme_location' => 'main-menu',
+				'walker' => new MDL_Walker(),
+				'items_wrap' => '<nav class="mdl-navigation">%3$s</nav>'
+				) ); ?>
+		</div>
+		<main class="mdl-layout__content">
+			<div class="page-content" id="container">
