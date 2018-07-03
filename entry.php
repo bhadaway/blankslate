4fd66f30dd84fabf3090c64e215cc3a899ae58b2
Annie Sullivan <sullivan@chromium.org>
2018-07-03 17:52:24 -0400

    Reformatted single posts so that they show as grid size 12

--- entry.php
+++ entry.php
@@ -1,12 +1,16 @@
-<article id="post-<?php the_ID(); ?>" class="<?php join( ' ', get_post_class() ); ?> mdl-cell--6-col mdl-card mdl-shadow--4dp">
+<?php if (is_singular()) {
+	$contents = get_the_content();
+	$grid_width = 12;
+} else {
+	$contents = get_the_excerpt();
+	$grid_width = 6;
+}?>
+<article id="post-<?php the_ID(); ?>"
+         class="<?php join( ' ', get_post_class() ); ?> mdl-cell--<?php echo $grid_width; ?>-col mdl-card mdl-shadow--4dp">
 <div class="mdl-card__media mdl-card--expand thumbnail-card mdl-color-text--grey-50" style="color: #fff; background-image: url(<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>)">
 	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><h3><?php the_title(); ?></h3></a>
 </div>
 <div class="mdl-card__supporting-text">
-<?php if (is_singular()) {
-	echo the_content();
-} else {
-	echo the_excerpt();
-}?>
+<?php echo $contents; ?>
 </div>
 </article>
