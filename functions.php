8d3b9f965d027dcfe8286b73802646e944a2fd76
Lee Collings <lee@leecollings.co>
2018-07-08 19:01:44 +0100

    Removed unneeded functions, load JS in footer by default, rename Widget area

--- functions.php
+++ functions.php
@@ -1,21 +1,20 @@
 <?php
 // General Setup
 function blankslate_setup() {
-  add_theme_support( 'title-tag' );
-  add_theme_support( 'automatic-feed-links' );
-  add_theme_support( 'post-thumbnails' );
-  global $content_width;
-  if ( ! isset( $content_width ) ) $content_width = 640;
-  register_nav_menus( array(
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  register_nav_menus(array(
     'main-menu' => 'Main Menu'
   ));
 }
-add_action( 'after_setup_theme', 'blankslate_setup' );
+add_action('after_setup_theme', 'blankslate_setup');
 
 // Styles & Scripts
 function blankslate_load_scripts() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_script( 'jquery' );
+  wp_enqueue_style('style', get_stylesheet_uri());
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), false, null, true);
+  wp_enqueue_script('jquery');
 }
 add_action( 'wp_enqueue_scripts', 'blankslate_load_scripts' );
 
@@ -23,8 +22,8 @@ add_action( 'wp_enqueue_scripts', 'blankslate_load_scripts' );
 // Setup Widgets
 function blankslate_widgets_init() {
   register_sidebar( array (
-    'name'          => 'Sidebar Widget Area',
-    'id'            => 'primary-widget-area',
+    'name'          => 'Widgets',
+    'id'            => 'widgets',
     'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
     'after_widget'  => "</li>",
     'before_title'  => '<h3 class="widget-title">',
