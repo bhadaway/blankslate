3de8f61b57573bcb1a59b6a8a1a00b433607a1e5
robertandrews <mail@robertandrews.co.uk>
2022-03-18 16:55:10 +0000

    Revert "Make excerpt W3C-valid by removing <p> from <span>"
    This reverts commit 6b3d41ccf4d350abba8577fb22489f628f8cc1d7.

--- entry-summary.php
+++ entry-summary.php
@@ -2,7 +2,7 @@
 <?php if ( ( has_post_thumbnail() ) && ( !is_search() ) ) : ?>
 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a>
 <?php endif; ?>
-<span itemprop="description"><?php echo(get_the_excerpt()); ?></span>
+<span itemprop="description"><?php the_excerpt(); ?></span>
 <?php if ( is_search() ) { ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 <?php } ?>
